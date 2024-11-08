051a1c030e31a4e5c9094986a9f4f5b1ad8192ef
le hungg <lehung020903@gmail.com>
2024-11-08 11:52:08 +0700

    update config sequelize

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_DRIVER, DB_PORT } =
-  process.env;
+const {
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_DRIVER,
+  DB_PORT,
+  DB_USERNAME_PRODUCTION,
+  DB_PASSWORD_PRODUCTION,
+  DB_DATABASE_PRODUCTION,
+  DB_DRIVER_PRODUCTION,
+  DB_HOST_PRODUCTION,
+  DB_PORT_PRODUCTION,
+} = process.env;
 
 module.exports = {
   development: {
@@ -20,11 +32,11 @@ module.exports = {
     port: DB_PORT,
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    dialect: DB_DRIVER,
-    port: DB_PORT,
+    username: DB_USERNAME_PRODUCTION,
+    password: DB_PASSWORD_PRODUCTION,
+    database: DB_DATABASE_PRODUCTION,
+    host: DB_HOST_PRODUCTION,
+    dialect: DB_DRIVER_PRODUCTION,
+    port: DB_PORT_PRODUCTION,
   },
 };
